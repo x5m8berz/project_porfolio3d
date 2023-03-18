22a5b5811cd73f5f9816c5bce367997f976aa396
Hanrocky <hanrocky.halim@gmail.com>
2023-03-18 10:04:22 +0700

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat"></div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
