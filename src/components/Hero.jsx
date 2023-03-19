a84d4935ad69b0a7093f34d74674a230dd6dd98b
Hanrocky <hanrocky.halim@gmail.com>
2023-03-19 22:52:06 +0700

    Added HOC

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-      <div className="relative bottom-32 xs:bottom-10  w-full flex justify-center items-center ">
+      <div className="absolute bottom-32 xs:bottom-10  w-full flex justify-center items-center ">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center p-2">
             <motion.div
--- src/constants/index.js
+++ src/constants/index.js
@@ -46,7 +46,7 @@ const services = [
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "Flutter Developer",
     icon: mobile,
   },
   {
@@ -54,7 +54,7 @@ const services = [
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Python Developer",
     icon: creator,
   },
 ];
