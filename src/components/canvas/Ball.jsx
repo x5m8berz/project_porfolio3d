4b48d3bfc216f7b517c23466ab0e260cd4af0fc5
Hanrocky <hanrocky.halim@gmail.com>
2023-03-23 20:27:01 +0700

    Added property for Ball component

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -16,6 +16,10 @@ const Ball = (props) => {
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+      </mesh>
     </Float>
   );
 };
