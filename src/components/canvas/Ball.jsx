1c3b0050431e1975ea557929ff5f89d47f295c5d
Hanrocky <hanrocky.halim@gmail.com>
2023-04-06 23:27:49 +0700

    Added property for the ball

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -25,6 +25,11 @@ const Ball = (props) => {
           polygonOffsetFactor={-5}
           flatShading
         />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          map={decal}
+        />
       </mesh>
     </Float>
   );
