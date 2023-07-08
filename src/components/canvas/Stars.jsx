4c5e36d1a532a39aeb59baae774baf63706056a2
Hanrocky <hanrocky.halim@gmail.com>
2023-07-08 12:02:38 +0700

    Finished Earth Canvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = () => {
+  return <div>Stars</div>;
+};
+
+const StarCanvas = () => {
   return (
-    <div>Stars</div>
+    <div></div>
   )
 }
 
-export default Stars
+export default Stars;
